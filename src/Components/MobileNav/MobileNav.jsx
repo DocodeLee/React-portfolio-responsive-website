72100d4bf37606b7fa245bcce6a3691204b0ce7d
DocodeLee <167724096+DocodeLee@users.noreply.github.com>
2024-05-17 03:04:16 +0900

    Update MobileNav.jsx

--- src/Components/MobileNav/MobileNav.jsx
+++ src/Components/MobileNav/MobileNav.jsx
@@ -14,18 +14,18 @@ const MobileNav = ({isOpen, toggleMenu}) => {
 
     <ul>
         <li>
-            <a className= "menu-item">Home</a>
+            <a href='https://www.youtube.com/' className= "menu-item">Home</a>
         </li>
         <li>
-        <a className='menu-item'>Skills</a>
+        <a href='https://www.youtube.com/'  className='menu-item'>Skills</a>
       </li>
 
       <li>
-        <a className='menu-item'>Work Experience</a>
+        <a href='https://www.youtube.com/' className='menu-item'>Work Experience</a>
       </li>
 
       <li>
-        <a className='menu-item'>Contact Me</a>
+        <a href='https://www.youtube.com/' className='menu-item'>Contact Me</a>
       </li>
       <button className='contact-btn' onClick={() => {}}>
         Hire Me
