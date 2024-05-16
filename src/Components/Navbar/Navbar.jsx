60183f56131cef3a532814c0b7dcff886e2f7cae
DocodeLee <167724096+DocodeLee@users.noreply.github.com>
2024-05-17 02:55:56 +0900

    Update Navbar.jsx

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -25,16 +25,16 @@ const Navbar = () => {
       <h1 className='logo'>Welcome To My HomePage</h1>
     <ul>
       <li>
-        <a className='menu-item'>Home</a>
+        <a href='https://www.youtube.com/' className='menu-item'>Home</a>
       </li>
       <li>
-        <a className='menu-item'>Skills</a>
+        <a href='https://www.youtube.com/' className='menu-item'>Skills</a>
       </li>
       <li>
-        <a className='menu-item'>Work Experience</a>
+        <a href='https://www.youtube.com/' className='menu-item'>Work Experience</a>
       </li>
       <li>
-        <a className='menu-item'>Contact Me</a>
+        <a href='https://www.youtube.com/' className='menu-item'>Contact Me</a>
       </li>
       <button className="contact-btn" onClick={()=>{}}>
         Hire Me
